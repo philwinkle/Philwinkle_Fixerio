a5e633098969efbde80c20492116bbaaa9c3cb3b
Andrea Zambon <azambon@webgriffe.com>
2021-06-10 16:17:39 +0200

    Moves the middle class to a separate file

--- app/code/community/Philwinkle/Fixerio/Model/Import.php
+++ app/code/community/Philwinkle/Fixerio/Model/Import.php
@@ -4,22 +4,13 @@
  *
  */
 
-//For Magento 1.9.4.3+ this model will extend Magento's implementation, which in turn extends the abstract class
-//For older Magento versions, this model extends the abstract class directly, as there is no Magento implementation
-//to extend
-if (class_exists('Mage_Directory_Model_Currency_Import_Fixerio')) {
-    abstract class Philwinkle_Fixerio_Model_Import_Base extends Mage_Directory_Model_Currency_Import_Fixerio {}
-} else {
-    abstract class Philwinkle_Fixerio_Model_Import_Base extends Mage_Directory_Model_Currency_Import_Abstract {}
-}
-
 /**
  * Philwinkle_Fixerio_Model_Import class
  *
  * @category    Philwinkle
  * @package     Philwinkle_Fixerio
  */
-class Philwinkle_Fixerio_Model_Import extends Philwinkle_Fixerio_Model_Import_Base
+class Philwinkle_Fixerio_Model_Import extends Philwinkle_Fixerio_Model_Base
 {
     /**
      * XML path to Fixer.IO timeout setting
